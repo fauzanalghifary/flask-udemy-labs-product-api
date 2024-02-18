d97de5970fd342f0cb8273c70650751575d10769
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-18 22:17:38 +0700

    finish task 2

--- test/test_product_categories_service.py
+++ test/test_product_categories_service.py
@@ -10,7 +10,8 @@ __author__ = "Michael Pogrebinsky - www.topdeveloperacademy.com"
 class TestProductCategoriesService(unittest.TestCase):
 
     def test_get_all_supported_categories(self):
-        database = Database()
+        database = Mock()
+        database.find_all_categories.return_value = ["electronics", "art", "toys"]
         service = ProductCategoriesService(database)
         expected_categories = "art,electronics,toys"
         self.assertEqual(service.get_all_supported_categories(), expected_categories)
